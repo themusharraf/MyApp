6028e05d37e6ab72e641d504b89790bd3d118378
allnc <meibrohimov@gmail.com>
2023-12-23 12:29:38 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6 import QtGui
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(QSize(500, 700))
+        self.setWindowIcon(QtGui.QIcon('icon.png'))
+        self.setWindowTitle("My App")
+
+        button = QPushButton("hello world")
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
